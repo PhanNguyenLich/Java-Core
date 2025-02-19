299aa5069f0c0c176ad39bd07f9aaea747a971f3
PhanNguyenLich <phanlich408@gmail.com>
2025-02-19 17:15:34 +0700

    Lab02 bai01 va 02

--- src/pieoit/Bai04.java
+++ src/pieoit/Bai04.java
@@ -4,19 +4,25 @@ import java.util.Scanner;
 
 public class Bai04 {
 	public static void main(String[] args) {
+		System.out.println(">> LỰA CHỌN TÍNH NĂNG <<");
+		System.out.println("++ ------------------ ++");
+		System.out.println("| 1. Cộng |");
+		System.out.println("| 2. Trừ |");
+		System.out.println("| 3. Kết thúc |");
+		System.out.println("++ ------------------ ++");
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Nhập tiền thuế của bạn: ");
-		int tax = scanner.nextInt();
-		if (tax < 10)
-			System.out.println("Không đóng thuế");
-		if (tax >= 10 && tax <= 15) {
-			System.out.println("Thuế 10%");
-		}
-		if (tax >= 15 && tax <= 30) {
-			System.out.println("Thuế 20%");
-		}
-		if (tax > 30) {
-			System.out.println("Thuế 50%");
+		System.out.println("Lựa chọn của bạn là : ");
+		int option = scanner.nextInt();
+		switch (option) {
+		case 1:
+			System.out.println("Bạn đã lựa chọn phép cộng");
+			break;
+		case 2:
+			System.out.println("Bạn đã lựa chọn phép trừ");
+			break;
+		default:
+			System.out.println("Bạn đã lựa chọn thoát chương trình");
+			System.exit(0);
 		}
 
 		scanner.close();
